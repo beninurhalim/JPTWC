02273d7fc10a22c3572d22de8fb4997257e8342f
Beni <beninurhalim@gmail.com>
2019-05-24 15:44:55 +0700

    udah firebase

--- app/src/main/java/dev/beni/myapplication/profile/Profile.java
+++ app/src/main/java/dev/beni/myapplication/profile/Profile.java
@@ -8,7 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 import dev.beni.myapplication.R;
+import dev.beni.myapplication.event.EventActivity;
+import dev.beni.myapplication.login.Home_screen;
+import dev.beni.myapplication.login.singin_activity;
 
 public class Profile extends Fragment {
 
@@ -16,6 +28,45 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+//        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button button = (Button) view.findViewById(R.id.signout);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()==null)
+                {
+                    Intent intent = new Intent(getContext(), singin_activity.class);
+//                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+
+            }
+        });
+
+        return view;
     }
+
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+
+
 }
